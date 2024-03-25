7d2cdbfdca17960be16d2cb63393bf6f5edce306
ty2004i <144071690+ty2004i@users.noreply.github.com>
2024-03-24 21:29:56 -0400

    chngement migrate_1.sql

---
+++ db_scripts/migrate_1.sql
@@ -0,0 +1,16 @@
+ALTER TABLE singer RENAME TO musician;
+ALTER TABLE musician ADD COLUMN role VARCHAR(50);
+ALTER TABLE musician ADD COLUMN bandName VARCHAR(50);
+UPDATE musician SET role = 'vocals', bandName = 'Crazy Duo' WHERE musicianName = 'Alina';
+UPDATE musician SET role = 'guitar', bandName = 'Mysterio' WHERE musicianName = 'Mysterio';
+UPDATE musician SET role = 'percussion', bandName = 'Crazy Duo' WHERE musicianName = 'Rainbow';
+UPDATE musician SET role = 'piano', bandName = 'Luna' WHERE musicianName = 'Luna';
+ALTER TABLE album RENAME COLUMN singerName TO musicianName;
+CREATE TABLE band (
+    bandName VARCHAR(50),
+    creation YEAR,
+    genre VARCHAR(50)
+);
+INSERT INTO band (bandName, creation, genre) VALUES ('Crazy Duo', 2015, 'rock');
+INSERT INTO band (bandName, creation, genre) VALUES ('Luna', 2009, 'classical');
+INSERT INTO band (bandName, creation, genre) VALUES ('Mysterio', 2019, 'pop');
